c701ad1b236ed512e22e0b7f1e095a81b2c01f96
Nikhil Bansal <nikhil97.nb@gmail.com>
2019-03-16 17:45:57 +0530

    Added BottomAppBar

--- app/src/main/java/com/app/nikhil/materialcomponetscatalog/MainActivity.kt
+++ app/src/main/java/com/app/nikhil/materialcomponetscatalog/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.app.nikhil.materialcomponetscatalog
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.app.nikhil.materialcomponetscatalog.R.drawable
+import com.app.nikhil.materialcomponetscatalog.R.id
+import com.app.nikhil.materialcomponetscatalog.fragments.BottomAppBarFragment
+import kotlinx.android.synthetic.main.activity_main.drawerLayout
+import kotlinx.android.synthetic.main.activity_main.navigationView
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    initToolbar()
+
+    navigationView.setNavigationItemSelectedListener { menuItem ->
+      menuItem.isChecked = true
+      drawerLayout.closeDrawers()
+      openFragment(menuItem)
+      true
+    }
+  }
+
+  private fun initToolbar() {
+    val toolbar: Toolbar = findViewById(id.toolbar)
+    setSupportActionBar(toolbar)
+    supportActionBar?.apply {
+      setDisplayHomeAsUpEnabled(true)
+      setHomeAsUpIndicator(drawable.ic_menu)
+    }
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when (item.itemId) {
+      android.R.id.home -> {
+        drawerLayout.openDrawer(GravityCompat.START)
+        true
+      }
+      else -> super.onOptionsItemSelected(item)
+    }
+  }
+
+  private fun openFragment(menuItem: MenuItem) {
+    val fragment = when (menuItem.itemId) {
+      R.id.itemBottomAppBar -> BottomAppBarFragment()
+      else -> null
+    }
+    fragment?.let {
+      supportFragmentManager.beginTransaction()
+          .replace(R.id.fragmentContainer, it)
+          .commit()
+    }
   }
 }
---
+++ app/src/main/java/com/app/nikhil/materialcomponetscatalog/fragments/BottomAppBarFragment.kt
@@ -0,0 +1,22 @@
+package com.app.nikhil.materialcomponetscatalog.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.app.nikhil.materialcomponetscatalog.R
+
+class BottomAppBarFragment : Fragment() {
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    val view = inflater.inflate(R.layout.fragment_bottom_app_bar, container, false)
+
+    return view
+  }
+}
+
